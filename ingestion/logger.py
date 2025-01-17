2a7c682af8f1ce0c2dcd4d41f8d963a30a3d123d
Edgaras Gacionis <gyvenimasritmu@gmail.com>
2025-01-17 19:08:30 +0200

    aws logging

--- ingestion/logger.py
+++ ingestion/logger.py
@@ -1,28 +1,18 @@
 import logging
-from logging.handlers import RotatingFileHandler
 import os
 
 # Environment configurations for logging
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
-LOG_FILE = os.getenv("LOG_FILE", "app.log")
-LOG_MAX_BYTES = int(os.getenv("LOG_MAX_BYTES", 10 * 1024 * 1024))  # 10 MB
-LOG_BACKUP_COUNT = int(os.getenv("LOG_BACKUP_COUNT", 5))
 
 # Configure logging format
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 # Initialize the root logger
 logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
+
 logger = logging.getLogger("ApplicationLogger")
 
-# Add a rotating file handler
-file_handler = RotatingFileHandler(
-    LOG_FILE, maxBytes=LOG_MAX_BYTES, backupCount=LOG_BACKUP_COUNT
-)
-file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
-logger.addHandler(file_handler)
-
-# Add a console handler for stdout logging
+# Add a console handler (stdout logging)
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 logger.addHandler(console_handler)
